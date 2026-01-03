aaf5c1878c0d4c9265fc45ff65d89f3695827066
Yosshy's Stash <Yosshy_123-sub@proton.me>
2026-01-03 11:51:53 +0900

    Update ipdmJDnfs.js

--- windows/ipdmJDnfs.js
+++ windows/ipdmJDnfs.js
@@ -1,6 +1,4 @@
   const now = new Date();
   const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const userAgent = navigator.userAgent;
-  document.getElementById("useragent").textContent = "User Agent: " + userAgent;
-  document.getElementById("datetime").textContent = "Current Time: " + now.toLocaleString();
-  document.getElementById("timezone").textContent = "Time Zone: " + timeZone;
+  document.getElementById("datetime").textContent = "日時: " + now.toLocaleString();
+  document.getElementById("timezone").textContent = "タイムゾーン: " + timeZone;
